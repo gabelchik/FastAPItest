2dad45a202f805a5e17db6c0ec3642b47c9f2456
Leonid Kalintsev <lensuperhero@gmail.com>
2026-03-23 13:15:28 +0300

    Files+

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-import asyncio
-import time
-
-from fastapi import FastAPI, BackgroundTasks
-
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import StreamingResponse
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
-def sync_task():
-    time.sleep(3)
-    print("Отправлен email")
+
+@app.post("/files")
+async def upload_file(uploaded_file: UploadFile):
+    file = uploaded_file.file
+    filename = uploaded_file.filename
+    with open(f"1_{filename}", "wb") as f:
+        f.write(file.read())
 
 
-async def async_task():
-    await asyncio.sleep(3)
-    print("Сделан запрос в сторонний API")
+@app.post("/multiple_files")
+async def upload_files(uploaded_files: list[UploadFile]):
+    for uploaded_file in uploaded_files:
+        file = uploaded_file.file
+        filename = uploaded_file.filename
+        with open(f"1_{filename}", "wb") as f:
+            f.write(file.read())
 
 
-@app.post("/")
-async def some_route(bg_tasks: BackgroundTasks):
-    ...
-    # asyncio.create_task(async_task())
-    bg_tasks.add_task(sync_task)
-    return {"ok": True}
+@app.get("/files/{filename}")
+async def get_file(filename: str):
+    return FileResponse(filename)
+
+
+def iterfile(filename: str):
+    with open(filename, "rb") as f:
+        while chunk := f.read(1024 * 1024):
+            yield chunk
+
+
+@app.get("/files/streaming/{filename}")
+async def get_streaming_file(filename: str):
+    return StreamingResponse(iterfile(filename), media_type="video/mp4")
